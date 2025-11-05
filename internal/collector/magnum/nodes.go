1d8e6b32e5ac590482ec713387db227541b1fd45
Tadas Sutkaitis <tadas.sutkaitis@vexxhost.com>
2025-11-05 18:04:04 +0200

    feat: split
    Signed-off-by: Tadas Sutkaitis <tadas.sutkaitis@vexxhost.com>

---
+++ internal/collector/magnum/nodes.go
@@ -0,0 +1,107 @@
+package magnum
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log/slog"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/vexxhost/openstack_database_exporter/internal/collector"
+	magnumdb "github.com/vexxhost/openstack_database_exporter/internal/db/magnum"
+)
+
+var (
+	clusterNodesCountDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(collector.Namespace, Subsystem, "cluster_nodes"),
+		"cluster_nodes",
+		[]string{
+			"uuid",
+			"name",
+			"stack_id",
+			"status",
+			"master_count",
+			"project_id",
+		},
+		nil,
+	)
+)
+
+type NodesCollector struct {
+	db     *sql.DB
+	logger *slog.Logger
+}
+
+func NewNodesCollector(db *sql.DB, logger *slog.Logger) *NodesCollector {
+	return &NodesCollector{
+		db:     db,
+		logger: logger,
+	}
+}
+
+func (c *NodesCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- clusterNodesCountDesc
+}
+
+func (c *NodesCollector) Collect(ch chan<- prometheus.Metric) {
+	ctx := context.Background()
+	queries := magnumdb.New(c.db)
+
+	c.collectNodeMetrics(ctx, queries, ch)
+}
+
+func (c *NodesCollector) collectNodeMetrics(ctx context.Context, queries *magnumdb.Queries, ch chan<- prometheus.Metric) {
+	clusters, err := queries.GetClusterMetrics(ctx)
+	if err != nil {
+		c.logger.Error("Failed to get cluster metrics for nodes", "error", err)
+		return
+	}
+
+	// Individual cluster node metrics
+	for _, cluster := range clusters {
+		uuid := ""
+		if cluster.Uuid.Valid {
+			uuid = cluster.Uuid.String
+		}
+
+		name := ""
+		if cluster.Name.Valid {
+			name = cluster.Name.String
+		}
+
+		projectID := ""
+		if cluster.ProjectID.Valid {
+			projectID = cluster.ProjectID.String
+		}
+
+		// Convert interface{} to int for counts
+		masterCount := 0
+		if cluster.MasterCount != nil {
+			if mc, ok := cluster.MasterCount.(int64); ok {
+				masterCount = int(mc)
+			}
+		}
+
+		nodeCount := 0
+		if cluster.NodeCount != nil {
+			if nc, ok := cluster.NodeCount.(int64); ok {
+				nodeCount = int(nc)
+			}
+		}
+
+		masterCountStr := fmt.Sprintf("%d", masterCount)
+
+		// cluster_nodes metric
+		ch <- prometheus.MustNewConstMetric(
+			clusterNodesCountDesc,
+			prometheus.GaugeValue,
+			float64(nodeCount),
+			uuid,
+			name,
+			cluster.StackID,
+			cluster.Status,
+			masterCountStr,
+			projectID,
+		)
+	}
+}
